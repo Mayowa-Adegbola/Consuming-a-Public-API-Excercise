600d8383828c533a72b9fcbf5db0a54c8c3d0e92
Mayowa <mayowaadegbola.r@gmail.com>
2016-05-20 19:11:43 +0100

    correct error in comment string

--- movie.js
+++ movie.js
@@ -19,12 +19,11 @@ $(document).ready(function(){
 
             $('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
 
-            $.getJSON("http://api.themoviedb.org/3/search/movie?api_key=a12e84be9ffed661d18b00b87ceffd35/" + film + "?callback=?", function(json) {
+            $.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/a12e84be9ffed661d18b00b87ceffd35/" + film + "?callback=?", function(json) {
                if (json != "Nothing found."){
                      $('#poster').html('<h2 class="loading">Well, gee whiz! We found you a poster, skip!</h2><img id="thePoster" src=' + json[0].posters[0].image.url + ' />');
                   } else {
-                     $.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/
-a12e84be9ffed661d18b00b87ceffd35/goonies?callback=?", function(json) {
+                     $.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/a12e84be9ffed661d18b00b87ceffd35/goonies?callback=?", function(json) {
                         console.log(json);
                         $('#poster').html('<h2 class="loading">We\'re afraid nothing was found for that search. Perhaps you were looking for The Goonies?</h2><img id="thePoster" src=' + json[0].posters[0].image.url + ' />');
                      });
